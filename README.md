cad7b78ff9dec1d0aba9b7ba892d94e76af6bd52
walkowif <59475134+walkowif@users.noreply.github.com>
2023-10-13 10:59:36 +0200

    Initialize repo (#1)

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # locksmith
-renv.lock generator
+
+[![build](https://github.com/insightsengineering/locksmith/actions/workflows/build.yml/badge.svg)](https://github.com/insightsengineering/locksmith/actions/workflows/build.yml)
+
+`locksmith` is a utility to generate `renv.lock` file containing all dependencies of given set of R packages.
+
+Given the input list of R packages or git repositories containing the R packages, as well as a list of R package repositories (e.g. in a package manager, CRAN, BioConductor etc.), `locksmith` will try to determine the list of all dependencies and their versions required to make the input list of packages work. It will then save the result in an `renv.lock`-compatible file.
+
+## Installing
+
+Simply download the project for your distribution from the [releases](https://github.com/insightsengineering/locksmith/releases) page. `locksmith` is distributed as a single binary file and does not require any additional system requirements.
+
+## Usage
+
+`locksmith` is a command line utility, so after installing the binary in your `PATH`, simply run the following command to view its capabilities:
+
+```bash
+locksmith --help
+```
+
+Example usage with multiple flags:
+```bash
+locksmith --logLevel debug --exampleParameter 'exampleValue'
+```
+
+## Configuration file
+
+If you'd like to set the above options in a configuration file, by default `locksmith` checks `~/.locksmith`, `~/.locksmith.yaml` and `~/.locksmith.yml` files.
+If this file exists, `locksmith` uses options defined there, unless they are overridden by command line flags.
+
+You can also specify custom path to configuration file with `--config <your-configuration-file>.yml` command line flag.
+When using custom configuration file, if you specify command line flags, the latter will still take precedence.
+
+Example contents of configuration file:
+
+```yaml
+logLevel: trace
+exampleParameter: exampleValue
+```
+
+## Environment variables
+
+`locksmith` reads environment variables with `LOCKSMITH_` prefix and tries to match them with CLI flags.
+For example, setting the following variables will override the respective values from configuration file:
+`LOCKSMITH_LOGLEVEL`, `LOCKSMITH_EXAMPLEPARAMETER` etc.
+
+The order of precedence is:
+
+CLI flag → environment variable → configuration file → default value.
+
+## Development
+
+This project is built with the [Go programming language](https://go.dev/).
+
+### Development Environment
+
+It is recommended to use Go 1.21+ for developing this project. This project uses a pre-commit configuration and it is recommended to [install and use pre-commit](https://pre-commit.com/#install) when you are developing this project.
+
+### Common Commands
+
+Run `make help` to list all related targets that will aid local development.
+
+## License
+
+`locksmith` is licensed under the Apache 2.0 license. See [LICENSE](LICENSE) for details.
---
+++ cmd/root.go
@@ -0,0 +1,147 @@
+/*
+Copyright 2023 F. Hoffmann-La Roche AG
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jamiealquiza/envy"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.szostok.io/version/extension"
+)
+
+var cfgFile string
+var logLevel string
+var exampleParameter string
+
+var log = logrus.New()
+
+func setLogLevel() {
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.ForceColors = true
+	log.SetFormatter(customFormatter)
+	log.SetReportCaller(true)
+	customFormatter.FullTimestamp = true
+	fmt.Println("logLevel =", logLevel)
+	switch logLevel {
+	case "trace":
+		log.SetLevel(logrus.TraceLevel)
+	case "debug":
+		log.SetLevel(logrus.DebugLevel)
+	case "info":
+		log.SetLevel(logrus.InfoLevel)
+	case "warn":
+		log.SetLevel(logrus.WarnLevel)
+	case "error":
+		log.SetLevel(logrus.ErrorLevel)
+	default:
+		log.SetLevel(logrus.InfoLevel)
+	}
+}
+
+var rootCmd *cobra.Command
+
+func newRootCommand() {
+	rootCmd = &cobra.Command{
+		Use:   "locksmith",
+		Short: "renv.lock generator",
+		Long: `locksmith is a utility to generate renv.lock file containing all dependencies of given set of R packages.
+Given the input list of R packages or git repositories containing the R packages, as well as a list of R package
+repositories (e.g. in a package manager, CRAN, BioConductor etc.), locksmith will try to determine the list of
+all dependencies and their versions required to make the input list of packages work.
+It will then save the result in an renv.lock-compatible file.`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			initializeConfig()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			setLogLevel()
+
+			fmt.Println("config =", cfgFile)
+			fmt.Println("exampleParameter =", exampleParameter)
+		},
+	}
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+		"config file (default is $HOME/.locksmith.yaml)")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "logLevel", "info",
+		"Logging level (trace, debug, info, warn, error). ")
+	rootCmd.PersistentFlags().StringVar(&exampleParameter, "exampleParameter", "",
+		"Example parameter that does nothing.")
+
+	// Add version command.
+	rootCmd.AddCommand(extension.NewVersionCobraCmd())
+
+	cfg := envy.CobraConfig{
+		Prefix:     "LOCKSMITH",
+		Persistent: true,
+	}
+	envy.ParseCobra(rootCmd, cfg)
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".locksmith" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".locksmith")
+	}
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func Execute() {
+	newRootCommand()
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func initializeConfig() {
+	for _, v := range []string{
+		"logLevel",
+		"exampleParameter",
+	} {
+		// If the flag has not been set in newRootCommand() and it has been set in initConfig().
+		// In other words: if it's not been provided in command line, but has been
+		// provided in config file.
+		// Helpful project where it's explained:
+		// https://github.com/carolynvs/stingoftheviper
+		if !rootCmd.PersistentFlags().Lookup(v).Changed && viper.IsSet(v) {
+			err := rootCmd.PersistentFlags().Set(v, fmt.Sprintf("%v", viper.Get(v)))
+			checkError(err)
+		}
+	}
+}
